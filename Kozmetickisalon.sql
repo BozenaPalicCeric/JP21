123fd2e9a9c1cf62a4beed8338ec16a709a08e7e
BozenaPalicCeric <57689147+BozenaPalicCeric@users.noreply.github.com>
2019-11-19 11:46:34 +0100

    Add files via upload

---
+++ Kozmetickisalon.sql
@@ -0,0 +1,58 @@
+drop database if exists kozmetickisalon;
+create database kozmetickisalon;
+use kozmetickisalon;
+
+create table djelatnica (
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    oib char (11) null
+);
+
+create table korisnik (
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    kontakt varchar(50) not null,
+    djelatnica int not null
+);
+
+create table posjeta (
+    sifra int not null primary key auto_increment,
+    usluga int not null,
+    korisnik int not null,
+    djelatnica int not null
+);
+
+create table usluga (
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null,
+    cijena decimal (18,2) not null,
+    trajanje datetime
+);
+
+alter table korisnik add foreign key (djelatnica) references djelatnica (sifra);
+
+alter table posjeta add foreign key (djelatnica) references djelatnica (sifra);
+alter table posjeta add foreign key (korisnik) references korisnik (sifra);
+alter table posjeta add foreign key (usluga) references usluga (sifra);
+
+insert into djelatnica (sifra,ime,prezime) values
+(null,'Mara', 'Marić'),
+(null, 'Petra', 'Petrić'),
+(null,'Maja','Majić');
+
+insert into korisnik (sifra,ime, prezime, kontakt,djelatnica) values
+(null, 'Nina', 'Petrović','nina@gmail.com',1),
+(null, 'Karin', 'Mijić', 'karin@gmail.com',2),
+(null, 'Ivka', 'Ivić', 'ivka@gmail.com',3);
+
+insert into usluga (sifra, naziv, cijena) values
+(null,'depilacija nogu','50.00'),
+(null, 'čišćenje lica', '100.00'),
+(null, 'masaža', '120.00');
+
+insert into posjeta (sifra, usluga, korisnik, djelatnica) values
+(null,1,2,3),
+(null,2,3,1),
+(null,3,1,2);
